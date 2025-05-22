8da19a95114da8378f971893f87881539d055147
PH Oliveira <132354546+phmoliv@users.noreply.github.com>
2025-05-22 10:57:11 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,15 @@
 variable "aws_region" {
-  type = string
   default = "sa-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID para a instância EC2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Nome da chave SSH existente na sua conta AWS"
 }
